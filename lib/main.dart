b819a0ad47051ac7874699e5d4b1f753bd050e5e
Pankaj Bhati <4121998bhati@gmail.com>
2021-05-07 01:35:54 +0530

    duration for notification feature has added

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,6 @@ class HomePage extends StatefulWidget {
   HomePageState createState() => HomePageState();
 }
 
-// List states = [];
-
-// var districts = [Districts("South Delhi", 149), Districts("East Delhi", 145)];
-
 class HomePageState extends State<HomePage> {
   static String _value = " ";
   static String districtName = "Select District";
@@ -26,11 +22,21 @@ class HomePageState extends State<HomePage> {
   List data;
   String url;
   int indexOfList;
-  int totalVaccines = 0;
+  List durations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  // int totalVaccines = 0;
   String stateName = "Select State";
   bool isCompleted = true;
   bool isStateSelected = false;
   bool isDistrictSelected = false;
+  bool isTimeOn = false;
+  Timer _timer;
+  int numberOfCovaxin = 0;
+  int numberOfCovishield = 0;
+  int count = 0;
+  bool isDurationSelected = false;
+  int duration;
+  bool pressNotify = false;
 
   @override
   void initState() {
@@ -47,14 +53,32 @@ class HomePageState extends State<HomePage> {
     setState(() {
       var convertDataToJson = json.decode(response.body);
       data = convertDataToJson['sessions'];
+      numberOfCovaxin = 0;
+      numberOfCovishield = 0;
       if (data != null && data.length != 0) {
         data.forEach((element) {
-          totalVaccines += element['available_capacity'].round();
+          if (element['vaccine'] == "COVAXIN")
+            numberOfCovaxin += element['available_capacity'].round();
+          else if (element['vaccine'] == "COVISHIELD") {
+            numberOfCovishield += element['available_capacity'].round();
+          }
         });
       }
       isCompleted = true;
     });
     return "Success";
+  }
+
+  void call_API_with_time(int duration) {
+    count = 0;
+    if (isTimeOn == true)
+      _timer = Timer.periodic(Duration(seconds: duration), (timer) {
+        print("${count++}");
+        isCompleted = false;
+        setState(() {
+          getJsonData();
+        });
+      });
   }
 
   @override
@@ -87,8 +111,10 @@ class HomePageState extends State<HomePage> {
                         },
                       ),
                       onChanged: (value) {
+                        if (_timer != null) _timer.cancel();
                         isStateSelected = true;
                         isDistrictSelected = false;
+                        pressNotify = false;
                         setState(() {
                           districts = allDistricts[value - 1];
                         });
@@ -114,25 +140,73 @@ class HomePageState extends State<HomePage> {
                         },
                       ),
                       onChanged: (value) {
+                        if (_timer != null) _timer.cancel();
+                        pressNotify = false;
                         setState(() {
                           _value = value.toString();
-                          isCompleted = false;
+                          // isCompleted = false;
                           isDistrictSelected = true;
                           isStateSelected = true;
-                          totalVaccines = 0;
-                          url =
-                              "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=$_value&date=06-05-2021";
-                          getJsonData();
                         });
                       }),
                 ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: DropdownButton(
+                      // value: _value,
+                      hint: isDurationSelected == false
+                          ? Text('Duration of Notification')
+                          : Text('$duration'),
+                      items: List.generate(
+                        durations.length,
+                        (index) {
+                          return DropdownMenuItem(
+                            child: Text('${durations[index]}'),
+                            value: durations[index],
+                            onTap: () {
+                              duration = durations[index];
+                            },
+                          );
+                        },
+                      ),
+                      onChanged: (value) {
+                        if (_timer != null) _timer.cancel();
+                        pressNotify = false;
+                        setState(() {
+                          duration = value;
+                          isDurationSelected = true;
+                        });
+                      }),
+                ),
+                RaisedButton(
+                  child: Text("Notify"),
+                  onPressed: isStateSelected == false ||
+                          isDistrictSelected == false ||
+                          isDurationSelected == false
+                      ? null
+                      : () {
+                          if (_timer != null) _timer.cancel();
+                          setState(() {
+                            isCompleted = false;
+                            isTimeOn = true;
+                            pressNotify = true;
+                            url =
+                                "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=$_value&date=07-05-2021";
+                            getJsonData();
+                            call_API_with_time(duration);
+                          });
+                        },
+                )
               ],
             ),
             TotalVaccines(
-              totalVaccines: totalVaccines,
+              numberOfCovaxin: numberOfCovaxin,
+              numberOfCovishield: numberOfCovishield,
               isStateSelected: isStateSelected,
               isDistrictSelected: isDistrictSelected,
               isCompleted: isCompleted,
+              isDurationSelected: isDurationSelected,
+              pressNotify: pressNotify,
             )
           ],
         ));
@@ -140,16 +214,22 @@ class HomePageState extends State<HomePage> {
 }
 
 class TotalVaccines extends StatelessWidget {
-  int totalVaccines;
+  int numberOfCovaxin;
+  int numberOfCovishield;
   bool isStateSelected;
   bool isDistrictSelected;
   bool isCompleted;
+  bool isDurationSelected;
+  bool pressNotify;
 
   TotalVaccines(
-      {this.totalVaccines,
+      {this.numberOfCovaxin,
+      this.numberOfCovishield,
       this.isStateSelected,
       this.isDistrictSelected,
-      this.isCompleted});
+      this.isCompleted,
+      this.isDurationSelected,
+      this.pressNotify});
 
   @override
   Widget build(BuildContext context) {
@@ -162,227 +242,14 @@ class TotalVaccines extends StatelessWidget {
           ? Text("Please select state")
           : isDistrictSelected == false
               ? Text("Please select district")
-              : Container(
-                  child: Text('$totalVaccines'),
-                ),
+              : isDurationSelected == false
+                  ? Text("Please select duration")
+                  : pressNotify == false
+                      ? Text("Please press Notify button")
+                      : Container(
+                          child: Text(
+                              'Covishield: $numberOfCovishield\n Covaxin: $numberOfCovaxin'),
+                        ),
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'dart:async';
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
-// import 'Product.dart';
-//
-// void main() => runApp(MyApp(products: fetchProducts()));
-//
-// List<Product> parseProducts(String responseBody) {
-//   final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
-//   return parsed.map<Product>((json) => Product.fromMap(json)).toList();
-// }
-// Future<List<Product>> fetchProducts() async {
-//   final response = await http.get('api.openweathermap.org/data/2.5/box/city?bbox=12,32,15,37,10&appid=5e113a17c9fe40286c134139fd1eb996');
-//   if (response.statusCode == 200) {
-//     print(response.body);
-//     return parseProducts(response.body);
-//   } else {
-//     throw Exception('Unable to fetch products from the REST API');
-//   }
-// }
-// class MyApp extends StatelessWidget {
-//   final Future<List<Product>> products;
-//   MyApp({Key key, this.products}) : super(key: key);
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: MyHomePage(title: 'Product Navigation demo home page', products: products),
-//     );
-//   }
-// }
-// class MyHomePage extends StatelessWidget {
-//   final String title;
-//   final Future<List<Product>> products;
-//   MyHomePage({Key key, this.title, this.products}) : super(key: key);
-//
-//   // final items = Product.getProducts();
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(title: Text("Product Navigation")),
-//         body: Center(
-//           child: FutureBuilder<List<Product>>(
-//             future: products, builder: (context, snapshot) {
-//             if (snapshot.hasError) print(snapshot.error);
-//             return snapshot.hasData ? ProductBoxList(items: snapshot.data) :
-//
-//             // return the ListView widget :
-//             Center(child: CircularProgressIndicator());
-//           },
-//           ),
-//         )
-//     );
-//   }
-// }
-// class ProductBoxList extends StatelessWidget {
-//   final List<Product> items;
-//   ProductBoxList({Key key, this.items});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       itemCount: items.length,
-//       itemBuilder: (context, index) {
-//         return ListTile(
-//           title: Text('Sun'),
-//         );
-//       },
-//     );
-//   }
-// }
-// // class ProductPage extends StatelessWidget {
-// //   ProductPage({Key key, this.item}) : super(key: key);
-// //   final Product item;
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return Scaffold(
-// //       appBar: AppBar(title: Text(this.item.name),),
-// //       body: Center(
-// //         child: Container(
-// //           padding: EdgeInsets.all(0),
-// //           child: Column(
-// //               mainAxisAlignment: MainAxisAlignment.start,
-// //               crossAxisAlignment: CrossAxisAlignment.start,
-// //               children: <Widget>[
-// //                 Image.asset("assets/appimages/" + this.item.image),
-// //                 Expanded(
-// //                     child: Container(
-// //                         padding: EdgeInsets.all(5),
-// //                         child: Column(
-// //                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-// //                           children: <Widget>[
-// //                             Text(this.item.name, style:
-// //                             TextStyle(fontWeight: FontWeight.bold)),
-// //                             Text(this.item.description),
-// //                             Text("Price: " + this.item.price.toString()),
-// //                             RatingBox(),
-// //                           ],
-// //                         )
-// //                     )
-// //                 )
-// //               ]
-// //           ),
-// //         ),
-// //       ),
-// //     );
-// //   }
-// // }
-// // class RatingBox extends StatefulWidget {
-// //   @override
-// //   _RatingBoxState createState() =>_RatingBoxState();
-// // }
-// // class _RatingBoxState extends State<RatingBox> {
-// //   int _rating = 0;
-// //   void _setRatingAsOne() {
-// //     setState(() {
-// //       _rating = 1;
-// //     });
-// //   }
-// //   void _setRatingAsTwo() {
-// //     setState(() {
-// //       _rating = 2;
-// //     });
-// //   }
-// //   void _setRatingAsThree() {
-// //     setState(() {
-// //       _rating = 3;
-// //     });
-// //   }
-// //   Widget build(BuildContext context) {
-// //     double _size = 20;
-// //     print(_rating);
-// //     return Row(
-// //       mainAxisAlignment: MainAxisAlignment.end,
-// //       crossAxisAlignment: CrossAxisAlignment.end,
-// //       mainAxisSize: MainAxisSize.max,
-// //
-// //       children: <Widget>[
-// //         Container(
-// //           padding: EdgeInsets.all(0),
-// //           child: IconButton(
-// //             icon: (
-// //                 _rating >= 1
-// //                     ? Icon(Icons.star, size: _size,)
-// //                     : Icon(Icons.star_border, size: _size,)
-// //             ),
-// //             color: Colors.red[500], onPressed: _setRatingAsOne, iconSize: _size,
-// //           ),
-// //         ),
-// //         Container(
-// //           padding: EdgeInsets.all(0),
-// //           child: IconButton(
-// //             icon: (
-// //                 _rating >= 2
-// //                     ? Icon(Icons.star, size: _size,)
-// //                     : Icon(Icons.star_border, size: _size, )
-// //             ),
-// //             color: Colors.red[500],
-// //             onPressed: _setRatingAsTwo,
-// //             iconSize: _size,
-// //           ),
-// //         ),
-// //         Container(
-// //           padding: EdgeInsets.all(0),
-// //           child: IconButton(
-// //             icon: (
-// //                 _rating >= 3 ?
-// //                 Icon(Icons.star, size: _size,)
-// //                     : Icon(Icons.star_border, size: _size,)
-// //             ),
-// //             color: Colors.red[500],
-// //             onPressed: _setRatingAsThree,
-// //             iconSize: _size,
-// //           ),
-// //         ),
-// //       ],
-// //     );
-// //   }
-// // }
-// // class ProductBox extends StatelessWidget {
-// //   ProductBox({Key key, this.item}) : super(key: key);
-// //   final Product item;
-// //
-// //   Widget build(BuildContext context) {
-// //     return Container(
-// //         padding: EdgeInsets.all(2), height: 140,
-// //         child: Card(
-// //           child: Row(
-// //               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-// //               children: <Widget>[
-// //                 Image.asset("assets/appimages/" + this.item.image),
-// //                 Expanded(
-// //                     child: Container(
-// //                         padding: EdgeInsets.all(5),
-// //                         child: Column(
-// //                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-// //                           children: <Widget>[
-// //                             Text(this.item.name, style:TextStyle(fontWeight: FontWeight.bold)),
-// //                             Text(this.item.description),
-// //                             Text("Price: " + this.item.price.toString()),
-// //                             RatingBox(),
-// //                           ],
-// //                         )
-// //                     )
-// //                 )
-// //               ]
-// //           ),
-// //         )
-// //     );
-// //   }
-// // }
