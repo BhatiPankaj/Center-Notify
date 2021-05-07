5cefbedb0752643d0f5b9e30c8411c8460a9537f
Pankaj Bhati <4121998bhati@gmail.com>
2021-05-08 02:27:22 +0530

    working on UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
@@ -6,8 +8,14 @@ import 'package:center_notify/Districts.dart';
 import 'States.dart';
 
 void main() => runApp(MaterialApp(
+      theme: ThemeData(
+          primaryColor: Color.fromARGB(255, 51, 51, 61),
+          accentColor: Color.fromARGB(255, 9, 175, 121),
+          accentColorBrightness: Brightness.light),
+      // theme: ThemeData.dark(),
       home: HomePage(),
       debugShowCheckedModeBanner: false,
+      // themeMode: ThemeMode.dark,
     ));
 
 class HomePage extends StatefulWidget {
@@ -84,25 +92,44 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: new AppBar(
-          title: new Text("Retrieve JSON"),
-        ),
-        body: Column(
-          children: [
-            Row(
+        backgroundColor: Color.fromARGB(255, 51, 51, 61),
+        body: Center(
+          child: Container(
+            width: 300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
+                  decoration: BoxDecoration(
+                    // borderRadius: Radius,
+                    color: Color.fromARGB(255, 38, 40, 47),
+                  ),
+                  // color: Color.fromARGB(255, 38, 40, 47),
                   padding: EdgeInsets.all(20),
                   child: DropdownButton(
-                      // value: _value,
+                      isExpanded: true,
+                      iconDisabledColor:
+                          Theme.of(context).accentColor.withOpacity(0.2),
+                      iconEnabledColor: Theme.of(context).accentColor,
+                      dropdownColor: Color.fromARGB(255, 38, 40, 47),
+                      // autofocus: true,
                       hint: isStateSelected == false
-                          ? Text("Select State")
-                          : Text(stateName),
+                          ? Text(
+                              "Select State",
+                              style: TextStyle(color: Colors.white54),
+                            )
+                          : Text(
+                              stateName,
+                              style: TextStyle(color: Colors.white),
+                            ),
                       items: List.generate(
                         states.length,
                         (index) {
                           return DropdownMenuItem(
-                            child: Text(states[index]['state_name']),
+                            child: Text(
+                              states[index]['state_name'],
+                              style: TextStyle(color: Colors.white),
+                            ),
                             value: states[index]['state_id'],
                             onTap: () {
                               stateName = states[index]['state_name'];
@@ -121,17 +148,32 @@ class HomePageState extends State<HomePage> {
                       }),
                 ),
                 Container(
+                  color: Color.fromARGB(255, 38, 40, 47),
                   padding: EdgeInsets.all(20),
                   child: DropdownButton(
-                      // value: _value,
+                      isExpanded: true,
+                      iconDisabledColor:
+                          Theme.of(context).accentColor.withOpacity(0.2),
+                      iconEnabledColor: Theme.of(context).accentColor,
+                      dropdownColor: Color.fromARGB(255, 38,40,47),
+                      // autofocus: true,
                       hint: isDistrictSelected == false
-                          ? Text('Select District')
-                          : Text(districtName),
+                          ? Text(
+                              'Select District',
+                              style: TextStyle(color: Colors.white54),
+                            )
+                          : Text(
+                              districtName,
+                              style: TextStyle(color: Colors.white),
+                            ),
                       items: List.generate(
                         districts.length,
                         (index) {
                           return DropdownMenuItem(
-                            child: Text(districts[index]['district_name']),
+                            child: Text(
+                              districts[index]['district_name'],
+                              style: TextStyle(color: Colors.white),
+                            ),
                             value: districts[index]['district_id'],
                             onTap: () {
                               districtName = districts[index]['district_name'];
@@ -151,24 +193,43 @@ class HomePageState extends State<HomePage> {
                       }),
                 ),
                 Container(
+                  color: Color.fromARGB(255, 38, 40, 47),
                   padding: EdgeInsets.all(20),
                   child: DropdownButton(
+                      isExpanded: true,
+                      iconDisabledColor:
+                          Theme.of(context).accentColor.withOpacity(0.2),
+                      iconEnabledColor: Theme.of(context).accentColor,
+                      dropdownColor: Color.fromARGB(255, 38,40,47),
                       // value: _value,
+                      // autofocus: true,
                       hint: isDurationSelected == false
-                          ? Text('Duration of Notification')
-                          : Text('$duration'),
-                      items: List.generate(
-                        durations.length,
-                        (index) {
-                          return DropdownMenuItem(
-                            child: Text('${durations[index]}'),
-                            value: durations[index],
-                            onTap: () {
-                              duration = durations[index];
-                            },
-                          );
-                        },
-                      ),
+                          ? Text(
+                              'Duration of Notification',
+                              style: TextStyle(color: Colors.white54),
+                            )
+                          : Text(
+                              '$duration',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                      items: isStateSelected == false ||
+                              isDistrictSelected == false
+                          ? null
+                          : List.generate(
+                              durations.length,
+                              (index) {
+                                return DropdownMenuItem(
+                                  child: Text(
+                                    '${durations[index]}',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  value: durations[index],
+                                  onTap: () {
+                                    duration = durations[index];
+                                  },
+                                );
+                              },
+                            ),
                       onChanged: (value) {
                         if (_timer != null) _timer.cancel();
                         pressNotify = false;
@@ -179,7 +240,15 @@ class HomePageState extends State<HomePage> {
                       }),
                 ),
                 RaisedButton(
-                  child: Text("Notify"),
+                  color: Theme.of(context).accentColor,
+                  child: Text(
+                    "Notify",
+                    style: isStateSelected == false ||
+                            isDistrictSelected == false ||
+                            isDurationSelected == false
+                        ? TextStyle(color: Colors.white24)
+                        : TextStyle(color: Colors.black),
+                  ),
                   onPressed: isStateSelected == false ||
                           isDistrictSelected == false ||
                           isDurationSelected == false
@@ -191,24 +260,24 @@ class HomePageState extends State<HomePage> {
                             isTimeOn = true;
                             pressNotify = true;
                             url =
-                                "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=$_value&date=07-05-2021";
+                                "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=$_value&date=08-05-2021";
                             getJsonData();
                             call_API_with_time(duration);
                           });
                         },
+                ),
+                TotalVaccines(
+                  numberOfCovaxin: numberOfCovaxin,
+                  numberOfCovishield: numberOfCovishield,
+                  isStateSelected: isStateSelected,
+                  isDistrictSelected: isDistrictSelected,
+                  isCompleted: isCompleted,
+                  isDurationSelected: isDurationSelected,
+                  pressNotify: pressNotify,
                 )
               ],
             ),
-            TotalVaccines(
-              numberOfCovaxin: numberOfCovaxin,
-              numberOfCovishield: numberOfCovishield,
-              isStateSelected: isStateSelected,
-              isDistrictSelected: isDistrictSelected,
-              isCompleted: isCompleted,
-              isDurationSelected: isDurationSelected,
-              pressNotify: pressNotify,
-            )
-          ],
+          ),
         ));
   }
 }
@@ -234,21 +303,37 @@ class TotalVaccines extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (isCompleted == false) {
-      return CircularProgressIndicator();
+      return CircularProgressIndicator(
+        backgroundColor: Color.fromARGB(255, 9, 175, 121),
+      );
     }
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: isStateSelected == false
-          ? Text("Please select state")
+          ? Text(
+              "Please select state",
+              style: TextStyle(color: Colors.white),
+            )
           : isDistrictSelected == false
-              ? Text("Please select district")
+              ? Text(
+                  "Please select district",
+                  style: TextStyle(color: Colors.white),
+                )
               : isDurationSelected == false
-                  ? Text("Please select duration")
+                  ? Text(
+                      "Please select duration",
+                      style: TextStyle(color: Colors.white),
+                    )
                   : pressNotify == false
-                      ? Text("Please press Notify button")
+                      ? Text(
+                          "Please press Notify button",
+                          style: TextStyle(color: Colors.white),
+                        )
                       : Container(
                           child: Text(
-                              'Covishield: $numberOfCovishield\n Covaxin: $numberOfCovaxin'),
+                            'Covishield: $numberOfCovishield\n Covaxin: $numberOfCovaxin',
+                            style: TextStyle(color: Colors.white),
+                          ),
                         ),
     );
   }
